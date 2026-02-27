0a24411f83c180a62c29b67d7506d58c29e657f0
ruairi O'Donoghue <ruairi10112@gmail.com>
2026-02-27 04:13:39 +0000

    Up to template expression MVC 2

--- models/app-store.js
+++ models/app-store.js
@@ -1,10 +1,17 @@
 'use strict';
 
 import logger from '../utils/logger.js';
+import JsonStore from './json-store.js';
 
 const appStore = {
 
-  // functions that manage JSON data to be added 
+  store: new JsonStore('./models/app-store.json', { info: {} }),
+  collection: 'info',
+  array: 'creators',
+
+  getAppInfo() {
+    return this.store.findAll(this.collection);
+  },
 
 };
 
