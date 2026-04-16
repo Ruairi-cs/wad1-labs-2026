a768174a79e336b0acce0a8d15f8d4d81fc709af
(20118000) Ruairi O Donoghue <20118000@setu.ie>
2026-04-16 16:39:56 +0100

    Week 9 lab 1 stats complete

---
+++ controllers/stats.js
@@ -0,0 +1,56 @@
+"use strict";
+import logger from "../utils/logger.js";
+import playlistStore from "../models/playlist-store.js";
+
+const stats = {
+    createView(request, response) {
+        logger.info("Stats page loading!");
+        // app statistics calculations
+        const playlists = playlistStore.getAllPlaylists();
+
+        let numPlaylists = playlists.length;
+
+        let numSongs = playlists.reduce((total, playlist) => total + playlist.songs.length, 0);
+
+        let average = numPlaylists > 0 ? (numSongs / numPlaylists).toFixed(2) : 0;
+
+        let totalRating = playlists.reduce((total, playlist) => total + parseInt(playlist.rating), 0);
+        let avgRating = numPlaylists > 0 ? totalRating / numPlaylists : 0;
+
+        let mapped = playlists.map(playlist => playlist.rating);
+
+        let maxRating = Math.max(...mapped);
+
+        let maxRated = playlists.filter(playlist => playlist.rating === maxRating);
+
+        let favTitles = maxRated.map(item => item.title);
+
+        let mapped2 = playlists.map(playlist => playlist.songs.length);
+        let maxSongs = Math.max(...mapped2);
+
+        let mostSongs = playlists.filter(playlist => playlist.songs.length === maxSongs);
+
+        let largestTitles = mostSongs.map(item => item.title);
+
+        const statistics = {
+            displayNumPlaylists: numPlaylists,
+            displayNumSongs: numSongs,
+            displayAverage: average,
+            displayAvgRating: avgRating.toFixed(2),
+            highest: maxRating,
+            displayFav: favTitles,
+            largest: maxSongs,
+            displayLargest: largestTitles
+
+        }
+
+        const viewData = {
+            title: "Playlist App Statistics",
+            stats: statistics
+        };
+
+        response.render("stats", viewData);
+    },
+};
+
+export default stats;
